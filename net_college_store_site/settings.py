93427b1d75feedf097f2dc23945cfa5ef2b1fc3d
mehdi1361 <mhd.mosavi@gmail.com>
2023-11-16 01:53:56 -0800

    add static files

--- net_college_store_site/settings.py
+++ net_college_store_site/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for net_college_store_site project.
 
@@ -9,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = f"{BASE_DIR}/static"
+#STATICFILES_DIRS = [
+#        os.path.join(BASE_DIR, "static"),
+# ]
+#STATIC_ROOT = f"{BASE_DIR}/static"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = f"{BASE_DIR}/uploads"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
